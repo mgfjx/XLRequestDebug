593c3edf94a11b5a44588c6ad76e3b91f68d65e5
xxl <xiexiaolong.xxl@alibaba-inc.com>
2021-12-14 18:29:00 +0800

    dealloc

---
+++ Example/XLRequestDebug/NSURLSessionTask+XLDealloc.h
@@ -0,0 +1,17 @@
+//
+//  NSURLSessionTask+XLDealloc.h
+//  XLRequestDebug_Example
+//
+//  Created by mgfjx on 2021/12/14.
+//  Copyright © 2021 xxl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSURLSessionTask (XLDealloc)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/XLRequestDebug/NSURLSessionTask+XLDealloc.m
@@ -0,0 +1,17 @@
+//
+//  NSURLSessionTask+XLDealloc.m
+//  XLRequestDebug_Example
+//
+//  Created by mgfjx on 2021/12/14.
+//  Copyright © 2021 xxl. All rights reserved.
+//
+
+#import "NSURLSessionTask+XLDealloc.h"
+
+@implementation NSURLSessionTask (XLDealloc)
+
+- (void)dealloc {
+    NSLog(@"[%@ dealloc]", NSStringFromClass(self.class));
+}
+
+@end
